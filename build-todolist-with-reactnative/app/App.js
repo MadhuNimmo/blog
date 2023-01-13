023fa48596b3bd6b83c04d31b050e8fe1d9391c1
MadhuNimmo <mchak009@ucr.edu>
2023-01-13 14:46:38 -0800

    Revert "call-graph related code changes"
    This reverts commit 6da1d5520626613117c2c9469fb9cba39c602fc9.

--- build-todolist-with-reactnative/app/App.js
+++ build-todolist-with-reactnative/app/App.js
@@ -3,33 +3,10 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Home from "./screens/Home";
 import Comments from "./screens/Comments";
 import Login from "./screens/Login";
-import { useState, useEffect } from 'react';
-import * as FileSystem from 'expo-file-system';
-import { AppState } from 'react-native';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-	var [appState, setAppState] = useState(AppState.currentState);
-
-	useEffect(() => {
-	AppState.addEventListener('change', handleAppStateChange);
-
-	return () => {
-	AppState.removeEventListener('change', handleAppStateChange);
-	};
-	}, []);
-
-	var handleAppStateChange = (nextAppState) => {
-		if (nextAppState !== 'active') {
-		const analysisData = J$.analysis.endExecution();
-		FileSystem.writeAsStringAsync(FileSystem.documentDirectory + 'DCG.json', JSON.stringify(analysisData)).then(()=>{
-		alert('DCG written to '+ FileSystem.documentDirectory + 'DCG.json')
-		}).catch((e)=>{
-		alert(e)
-		}); 
-		}
-	}
 	return (
 		<NavigationContainer>
 			<Stack.Navigator>
